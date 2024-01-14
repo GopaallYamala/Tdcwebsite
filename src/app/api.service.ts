204d69dbe09004376f86cf8aa5290b6efdc534b8
GopaallYamala <Gopal.yamala@outlook.com>
2024-01-14 12:05:20 +0530

    addtiel

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'https://strapicms2024-v3-vz7tr.ondigitalocean.app/api/products?populate=*';
-  private apiToken = 'ebf9835b058d7e10f7de3b339b3c8f16a953f525a2bea9a7539b51ea5231844113060c15e4fc9c0bcfa560b394d680fa284520d7419294918899cf4832aa415c90cb0dcfe5b22c874960523abe34ef00d12e5c31f1bf0e50b1ce192cc915ad5cf4519256e08c3a15c5f69d3565a917165634af2158efce03e7d6985b27003dc5'; // Replace with your actual API token
+  private apiUrl = 'https://strapicms2024-v1.onrender.com/api/products?populate=*';
+  private apiToken = '0c1061fbd548b6efce625afe6156ddea4098b28dd891c2b9862dd12974d599b90fab6362b4994bc793aa81221f6d5633e57565c4c04575ba3e824c1867b909d3408a350396e3284de48fbee5fc0b0b6d014e8ece6a9bc403d02f663b0e28d78c658f3fed490f4f96a19c3b3fc64386250c3f7fe66e4be44b21bb2b3f349ac246'; // Replace with your actual API token
 
   constructor(private http: HttpClient) { }
 
